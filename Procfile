5ac75f55a0b44a0aae53a3652c63adbbb4f90424
Sng Haoren <snghaoren@gmail.com>
2020-06-16 15:37:27 +0800

    change to single worker

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -k eventlet salad_bowl:app
+web: gunicorn -k eventlet -w 1 salad_bowl:app
--- salad_bowl.py
+++ salad_bowl.py
@@ -1,7 +1,7 @@
 from app import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
 
 
 
