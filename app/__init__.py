caddba41f30db1956f5cfe0087cac9d3a47bb5bb
Sng Haoren <snghaoren@gmail.com>
2022-06-04 15:50:19 +0800

    Set samesite cookie header to lax

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "")
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 socketio = SocketIO(app, manage_session=False)
+print("Server starting on port: 5000")
 
 from app import routes, sockets
 
